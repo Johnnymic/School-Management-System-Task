ff7a959d5e7ded915398eb3fc7d4317754f4b0de
John Michael <johnny25580@gmail.com>
2023-01-31 20:26:42 -0800

    add person class

---
+++ Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    String name;
+    int age;
+    int id;
+
+   public Person(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/package1/Person.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Person {
+   private String name;
+   private int age;
+   private int id;
+
+    public Person(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
